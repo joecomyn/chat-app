509b2026f64b8c7acad3af663bd8bb9dc88fbc92
joe comyn <joecomyn@hotmail.com>
2024-08-27 00:08:33 +0100

    Allows users to view rooms once they have logged in and select which room they wish to chat in

--- src/app/room-selection/room-selection.component.ts
+++ src/app/room-selection/room-selection.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { chatRoom } from '../../models/chat-room';
+import { UserService } from '../../services/user.service';
+import { ChatService } from '../../services/chat-app.service';
 
 @Component({
   selector: 'app-room-selection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers: [],
   templateUrl: './room-selection.component.html',
   styleUrl: './room-selection.component.css'
 })
 export class RoomSelectionComponent {
 
   user: string = "";
+  rooms: chatRoom[] = [];
+  errorMessage: string = '';
 
-  constructor(private userService: UserService, private router: Router) {};
+  constructor(private userService: UserService, private chatService: ChatService, private router: Router) {};
 
   ngOnInit(): void {
     this.user = this.userService.getUsername();
-
+    this.chatService.getChatRooms().subscribe({
+      next: (rooms) => this.rooms = rooms,
+      error: (error) => this.errorMessage = `Error getting rooms`,
+      complete: () => console.log("completed")
+    });
   };
+
+  selectRoom(roomId: number): void {
+    this.router.navigate(['/rooms', roomId]);
+  }
 
 }
